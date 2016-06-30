5a5d31d9bcb47f090ddc2c564a4a8050396d0070
Antoine Tenart <antoine.tenart@ack.tf>
2016-06-30 19:30:02 +0200

    Implement the configuration parser
    Signed-off-by: Antoine Tenart <antoine.tenart@ack.tf>

--- acl.go
+++ acl.go
@@ -3,7 +3,9 @@
 package acl
 
 import (
+	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/mholt/caddy"
 	"github.com/mholt/caddy/caddyhttp/httpserver"
@@ -11,6 +13,91 @@ import (
 
 type ACL struct {
 	Next	httpserver.Handler
+	Config	[]ACLBlockConfig
+}
+
+// describe one acl{} configuration block
+type ACLBlockConfig struct {
+	Paths	[]string
+	Allow	[]*net.IPNet
+	Deny	[]*net.IPNet
+	Status	int
+}
+
+func parsePrefix(c *caddy.Controller) (*net.IPNet, error) {
+	if !c.NextArg() {
+		return nil, c.ArgErr()
+	}
+
+	_, ret, err := net.ParseCIDR(c.Val())
+	if err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+func parseConfigurationBlock(c *caddy.Controller) (ACLBlockConfig, error) {
+	var cfg ACLBlockConfig
+
+	// get acl{} block first arguments (base paths)
+	// TODO: sort the paths: most specific first
+	cfg.Paths = c.RemainingArgs()
+	if len(cfg.Paths) == 0 {
+		return cfg, c.ArgErr()
+	}
+
+	// navigate through statements in an acl{} block
+	for c.NextBlock() {
+		val := c.Val()
+
+		switch val {
+		case "allow":
+			rule, err := parsePrefix(c)
+			if err != nil {
+				return cfg, err
+			}
+
+			cfg.Allow = append(cfg.Allow, rule)
+		case "deny":
+			rule, err := parsePrefix(c)
+			if err != nil {
+				return cfg, err
+			}
+
+			cfg.Deny = append(cfg.Deny, rule)
+		case "status":
+			if !c.NextArg() {
+				return cfg, c.ArgErr()
+			}
+
+			status, _ := strconv.Atoi(c.Val())
+			if http.StatusText(status) == "" {
+				// invalid HTTP status code
+				return cfg, c.Err("acl: invalid status code")
+			}
+
+			cfg.Status = status
+		}
+	}
+
+	return cfg, nil
+}
+
+func parseConfiguration(c *caddy.Controller) ([]ACLBlockConfig, error) {
+	var config []ACLBlockConfig
+
+	// navigate through the acl{} blocks
+	for c.Next() {
+		blockConf, err := parseConfigurationBlock(c)
+		if err != nil {
+			return config, err
+		}
+
+		config = append(config, blockConf)
+	}
+
+	return config, nil
 }
 
 func (self ACL) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
@@ -18,10 +105,17 @@ func (self ACL) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 }
 
 func setup(c *caddy.Controller) error {
+	// parse Caddyfile for acl configuration statements
+	aclConf, err := parseConfiguration(c)
+	if err != nil {
+		return err
+	}
+
 	// create a new Caddy middleware
 	mw := func(next httpserver.Handler) httpserver.Handler {
 		return &ACL{
-			Next: next,
+			Next:	next,
+			Config:	aclConf,
 		}
 	}
 
